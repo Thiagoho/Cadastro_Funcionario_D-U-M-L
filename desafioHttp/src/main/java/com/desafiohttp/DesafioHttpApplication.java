4f0f8a6944c93bedb669e90c50498f08db5eb2e8
Thiago H O S <thiago2006_sales@hotmail.com>
2025-12-08 17:58:06 -0300

    Atualizando o pom.xml

--- desafioHttp/src/main/java/com/desafiohttp/DesafioHttpApplication.java
+++ desafioHttp/src/main/java/com/desafiohttp/DesafioHttpApplication.java
@@ -8,6 +8,7 @@ public class DesafioHttpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioHttpApplication.class, args);
+		System.out.println("Hello! ");
 	}
 
 }
