92fdebe0aa286ede63edfcd9404e4fa143c791d4
Thiago H O S <thiago2006_sales@hotmail.com>
2025-12-28 19:50:25 -0300

    Package ===::> Repository

--- CadastandoAluno/src/main/java/com/Cadastro/Aluno/repository/AlunoRepository.java
+++ CadastandoAluno/src/main/java/com/Cadastro/Aluno/repository/AlunoRepository.java
@@ -1,5 +1,12 @@
 package com.Cadastro.Aluno.repository;
+/* A interface AlunoRepository conecta sua entidade Aluno com o banco de dados.
+Ela não precisa de código dentro (por isso está vazia), pois o Spring Data JPA já fornece tudo automaticamente.
+ * */
 
-public interface AlunoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Cadastro.Aluno.model.Aluno;
+
+public interface AlunoRepository  extends JpaRepository<Aluno, Long>{
 
 }
