3a980f7c228821453d6262222554de3dbe80b169
Thiago H O S <thiago2006_sales@hotmail.com>
2025-12-28 19:27:45 -0300

    Primeiro commit de teste.

---
+++ CadastandoAluno/src/main/java/com/Cadastro/Aluno/CadastandoAlunoApplication.java
@@ -0,0 +1,14 @@
+package com.Cadastro.Aluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastandoAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastandoAlunoApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ CadastandoAluno/src/test/java/com/Cadastro/Aluno/CadastandoAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cadastro.Aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastandoAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
