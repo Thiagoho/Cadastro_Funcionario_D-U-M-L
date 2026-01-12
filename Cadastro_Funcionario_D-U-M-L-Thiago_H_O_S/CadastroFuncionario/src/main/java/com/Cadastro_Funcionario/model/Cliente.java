6d05aa5d3d9e74885ebf6ffa2bbe21874e543753
Thiago H O S <thiago2006_sales@hotmail.com>
2026-01-12 19:52:30 -0300

    Criando atributos, construtor e getters&setters

--- Cadastro_Funcionario_D-U-M-L-Thiago_H_O_S/CadastroFuncionario/src/main/java/com/Cadastro_Funcionario/model/Cliente.java
+++ Cadastro_Funcionario_D-U-M-L-Thiago_H_O_S/CadastroFuncionario/src/main/java/com/Cadastro_Funcionario/model/Cliente.java
@@ -1,5 +1,30 @@
 package com.Cadastro_Funcionario.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+@Table(name = "cliente", 
+				uniqueConstraints = {
+						@UniqueConstraint(columnNames= "email") 
+				})
 public class Cliente {
-
+	
+	private Long id;
+	
+	private String nome;
+	
+	private String email;
+	
+	private String telefone;
+	
+	public Cliente () {}
+	public Cliente(String nome, String email, String telefone) {
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
+	
 }
