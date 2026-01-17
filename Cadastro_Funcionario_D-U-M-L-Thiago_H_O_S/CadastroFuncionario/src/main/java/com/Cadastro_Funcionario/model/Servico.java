b9c57a2466af9adceec2e1d84504dac29acc28f7
Thiago H O S <thiago2006_sales@hotmail.com>
2026-01-17 04:47:23 -0300

    Concluídos a models + Repository.

--- Cadastro_Funcionario_D-U-M-L-Thiago_H_O_S/CadastroFuncionario/src/main/java/com/Cadastro_Funcionario/model/Servico.java
+++ Cadastro_Funcionario_D-U-M-L-Thiago_H_O_S/CadastroFuncionario/src/main/java/com/Cadastro_Funcionario/model/Servico.java
@@ -1,5 +1,25 @@
 package com.Cadastro_Funcionario.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Table(name = "servico")
 public class Servico {
+	
+	private Long id;
+	
+	private String nome;
+	
+	private BigDecimal preco;
+	
+	public Servico() {}
+	public Servico(String nome, BigDecimal preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
 
 }
